cc82640c980377bc2fc9cc6c0584867346d957ec
Ashish <ashish@192.168.1.5>
2025-01-19 02:29:52 +0530

    user model

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,1 +1,45 @@
 import mongoose, { Schema } from "mongoose";
+
+
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trime: true,
+        index: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trime: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"],
+
+    },
+    mobile_no: {
+        type: String,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+
+}, { timestamps: true })
+
+
+
+export const User = mongoose.model("User", userSchema);
