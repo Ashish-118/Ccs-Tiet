9f2a51a526bf88ff61f45725b2cbc8ecdc5427bd
Ashish <ashish@192.168.1.5>
2025-01-19 01:27:19 +0530

    done utils and database

---
+++ Backend/src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    // origin: 'http://localhost:5173',
+    credentials: true
+}))
+
+app.use(express.json({ limit: "16kb" }))
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+
+app.use(express.static("public"))
+app.use(cookieParser())
+
+// route import
+
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter)
+
+//    [   http://localhost:8000/api/vl/users/register  ] 
+
+
+export { app }
+
