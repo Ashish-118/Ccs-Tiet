c3b969925b9b1c56d50a4524d8ae836e2da04395
Bhrigu verma <146697095+bhrigu-verma@users.noreply.github.com>
2025-01-19 11:10:33 +0530

    rfr

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,10 +1,34 @@
 import logo from './logo.svg';
 import { Route, Routes } from "react-router-dom";
 import './App.css';
+import { ContainerScroll } from "./components/ui/container-scroll-animation";
 
 function App() {
   return (
     <div className="App">
+     <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="text-4xl font-semibold text-black dark:text-white">
+              Unleash the power of <br />
+              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                Scroll Animations
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <img
+          src={`/linear.webp`}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
